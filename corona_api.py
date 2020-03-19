1343d11114e68faeb0cc6328f98688380ab66e91
farisalasmary <farisalasmary@gmail.com>
2020-03-19 13:45:15 +0300

    Update corona_api.py

--- corona_api.py
+++ corona_api.py
@@ -18,32 +18,35 @@ def get_stats():
     
     html = res.text
     soup = BeautifulSoup(html, 'html.parser')
+    tables = {}
+    for table_id in ['main_table_countries_today', 'main_table_countries_yesterday']:
+        data_table = soup.find("table", {"id": table_id})
+        
+        header_cells = data_table.find('thead').find_all('th')
+        rows = data_table.find('tbody').find_all('tr')
+        
+        header_cells_values = []
+        for header_cell in header_cells:
+            header_cell_value = header_cell.text.replace('\xa0', ' ')
+            header_cells_values.append(header_cell_value)
+        
+        info_per_country = {}
+        for row in rows:
+            country_info = {}
+            country_name = None
+            for i, cell in enumerate(row.find_all('td')):
+                cell_value = cell.text.replace(',', '').replace('+', '').strip()
+                if i == 0:
+                    country_name = cell_value
+                    info_per_country[country_name] = {}
+                else:
+                    if cell_value == '':
+                        cell_value = '0'
+                    info_per_country[country_name][header_cells_values[i]] = cell_value
+        
+        tables[table_id] = info_per_country
     
-    data_table = soup.find("table", {"id": "main_table_countries_today"})
-    
-    header_cells = data_table.find('thead').find_all('th')
-    rows = data_table.find('tbody').find_all('tr')
-    
-    header_cells_values = []
-    for header_cell in header_cells:
-        header_cell_value = header_cell.text.replace('\xa0', ' ')
-        header_cells_values.append(header_cell_value)
-    
-    info_per_country = {}
-    for row in rows:
-        country_info = {}
-        country_name = None
-        for i, cell in enumerate(row.find_all('td')):
-            cell_value = cell.text.replace(',', '').replace('+', '').strip()
-            if i == 0:
-                country_name = cell_value
-                info_per_country[country_name] = {}
-            else:
-                if cell_value == '':
-                    cell_value = '0'
-                info_per_country[country_name][header_cells_values[i]] = cell_value
-    
-    return info_per_country
+    return tables
 
 ##############################################################################################################################
 
@@ -56,20 +59,21 @@ def get_stats_api():
 
 @app.route('/get_stats/<country>', methods=['GET'])
 def get_stats_country_api(country):
-    info_per_country = get_stats()
+    tables = get_stats()
     
-    country_info = {}
-    if country in info_per_country:
-        country_info[country] = info_per_country[country]
+    country_tables = {}
+    for table_id in tables:
+        info_per_country = tables[table_id]
+        country_info = {}
+        if country in info_per_country:
+            country_info[country] = info_per_country[country]
+        country_tables[table_id] = country_info
     
-    return jsonify(country_info)
+    return jsonify(country_tables)
 
 ##############################################################################################################################
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='1111', debug=True)
-
-
-
 
 
